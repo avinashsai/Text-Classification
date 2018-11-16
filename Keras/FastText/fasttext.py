0b4424ca69812897afc2da60bd6ad1e3bb146963
Avinash Madasu <avinash.sai001@gmail.com>
2018-11-16 23:19:44 +0530

    FastText in keras

--- Keras/FastText/fasttext.py
+++ Keras/FastText/fasttext.py
@@ -13,25 +13,23 @@ class fasttext():
 		self.epochs = epochs
 		self.mincount = mincount
 
-	def define_model(self):
-		model = FastText(min_count=self.mincount,window=self.windowsize,
-			size=self.vectorsize)
-		return model
 
-	def imdb_fasttext_model(self,imdb_corpus,imdb_train,imdb_test):
-		imdb_model = self.define_model()
-		imdb_model.build_vocab(imdb_corpus)
+	def imdb_fasttext_model(self,imdb_train):
+		words = []
+		for sentence in imdb_train:
+			words+=sentence.split()
 
-		total_corpus = imdb_train+imdb_test
-		imdb_model.train(total_corpus,total_examples=imdb_model.corpus_count,epochs=self.epochs)
+		imdb_model = FastText(words,min_count=self.mincount,size=self.vectorsize,
+			window=self.windowsize,iter=self.epochs)
 
 		return imdb_model
 
-	def rotten_fasttext_model(self,rotten_corpus,rotten_train,rotten_test):
-		rotten_model = self.define_model()
-		rotten_model.build_vocab(rotten_corpus)
+	def rotten_fasttext_model(self,rotten_train):
+		words = []
+		for sentence in rotten_train:
+			words+=sentence.split()
 
-		total_corpus = rotten_train+rotten_test
-		rotten_model.train(total_corpus,total_examples=rotten_model.corpus_count,epochs=self.epochs)
+		rotten_model = FastText(words,min_count=self.mincount,size=self.vectorsize,
+			window=self.windowsize,iter=self.epochs)
 
 		return rotten_model
